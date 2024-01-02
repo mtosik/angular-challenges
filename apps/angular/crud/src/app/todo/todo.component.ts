7e0c1ce19a2f76602c8b5e41dc84767386040497
mtosik <michal.tosik@wp.pl>
2024-01-02 11:31:58 +0100

    refactor: add disable button

--- apps/angular/crud/src/app/todo/todo.component.ts
+++ apps/angular/crud/src/app/todo/todo.component.ts
@@ -1,37 +1,53 @@
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, Input, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Store } from '@ngrx/store';
+import { filter } from 'rxjs';
 import { TodoInterface } from '../../models/todo.model';
 import {
   todoDeleteActionGroup,
   todoUpdateActionGroup,
 } from '../../store/actions/todo.actions';
 import { selectTodoLoaderState } from '../../store/reducers/todo.reducer';
-
 @Component({
   selector: 'app-todo',
   template: `
     {{ todo.title }}
-    <button (click)="update(todo)" [disabled]="loadingState()">Update</button>
-    <button (click)="delete(todo)" [disabled]="loadingState()">delete</button>
-    @if (loadingState()) {
+    <button (click)="update(todo)" [disabled]="isLoading">Update</button>
+    <button (click)="delete(todo)" [disabled]="isLoading">delete</button>
+    @if (isLoading) {
       <mat-spinner></mat-spinner>
     }
   `,
   standalone: true,
   imports: [MatProgressSpinnerModule],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   @Input({ required: true }) todo!: TodoInterface;
-  loadingState = this.store.selectSignal(selectTodoLoaderState);
+  isLoading = false;
+  destroyRef = inject(DestroyRef);
 
   constructor(private store: Store) {}
 
+  ngOnInit(): void {
+    this.store
+      .select(selectTodoLoaderState)
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        filter((state) => !state),
+      )
+      .subscribe((state) => {
+        this.isLoading = false;
+      });
+  }
+
   update(todo: TodoInterface) {
+    this.isLoading = true;
     this.store.dispatch(todoUpdateActionGroup.update({ todo }));
   }
 
   delete(todo: TodoInterface) {
+    this.isLoading = true;
     this.store.dispatch(todoDeleteActionGroup.delete({ todo }));
   }
 }
